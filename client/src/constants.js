c9ee924dfa058caacd7412d60e8294073bff3723
Bot Wea ID <91173360+botwea-ID@users.noreply.github.com>
2021-09-23 10:18:06 +0700

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 export const APP_NAME = 'Franky';
-export const repoName = 'https://github.com/Botwea-ID/video-chat';
+export const repoName = 'https://frangky.me';
 export const APP_URL = 'https://meet.frangky.me';
 export const BACKEND_URL = 'https://meet.frangky.me';
 // export const BACKEND_URL='http://localhost:5000';
