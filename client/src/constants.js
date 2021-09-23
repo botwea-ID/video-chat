9cfd652e3aba6d8df931eba9b7659cf954ee80e0
Bot Wea ID <91173360+botwea-ID@users.noreply.github.com>
2021-09-23 10:13:53 +0700

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
-export const APP_NAME = 'Merge';
-export const repoName = 'https://github.com/suresh213/video-chat';
-export const APP_URL = 'https://merge.netlify.app';
-export const BACKEND_URL = 'https://video-chat-meet-backend.herokuapp.com';
+export const APP_NAME = 'Franky';
+export const repoName = 'https://github.com/Botwea-ID/video-chat';
+export const APP_URL = 'https://meet.frangky.me';
+export const BACKEND_URL = 'https://meet.frangky.me';
 // export const BACKEND_URL='http://localhost:5000';
